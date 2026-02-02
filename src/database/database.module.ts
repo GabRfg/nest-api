9d42a65a98a2810c4506d604d3b1c09736485817
GabRfg <theghost947@gmail.com>
2026-02-02 09:38:28 -0300

    TypeOrm and Database connection properties, manage data in local host

---
+++ src/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { Course } from 'src/courses/entities/courses.entity';
+//import { Type } from 'class-transformer';
+//import { data } from 'framer-motion/client';
+import { DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'devtraining',
+  entities: [Course],
+  synchronize: true,
+};
+
+//Pega tudo que foi definido acima, e passa para o modulo do TypeOrm 
+@Module({
+  imports: [TypeOrmModule.forRootAsync({
+    useFactory: async() => {
+      return {
+        ...dataSourceOptions,
+      }
+    }
+  })],
+})
+
+export class DatabaseModule {}
