1712c5552d4e6373b8178273347fe35ec29034ac
GabRfg <theghost947@gmail.com>
2026-02-03 17:04:11 -0300

    Finally migration works =)

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,8 @@ import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1770139861229 } from "src/migrations/1770139861229-CreateCoursesTable";
 import { CreateTagsTable1770142304365 } from "src/migrations/1770142304365-CreateTagsTable";
 import { CreateCoursesTagsTable1770143960590 } from "src/migrations/1770143960590-CreateCoursesTagsTable"
+import { AddCoursesIdToCoursesTagsTable1770145880247 } from "src/migrations/1770145880247-AddCoursesIdToCoursesTagsTable";
+import { AddTagsCoursesTagsTable1770146988254 } from "src/migrations/1770146988254-AddTagsCoursesTagsTable";
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -10,7 +12,9 @@ export const dataSource = new DataSource({
   migrations: [
     CreateCoursesTable1770139861229,
     CreateTagsTable1770142304365,
-    CreateCoursesTagsTable1770143960590
+    CreateCoursesTagsTable1770143960590,
+    AddCoursesIdToCoursesTagsTable1770145880247,
+    AddTagsCoursesTagsTable1770146988254
 
   ], //pega o que esta no arquivo, propriedades de conexão, e aplicar ao database
 });
--- src/migrations/1770143960590-CreateCoursesTagsTable.ts
+++ src/migrations/1770143960590-CreateCoursesTagsTable.ts
@@ -5,7 +5,7 @@ export class CreateCoursesTagsTable1770143960590 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'courses_tags',
+        name: 'courses_tags_tags',
         columns: [
           {
             name: 'id',
